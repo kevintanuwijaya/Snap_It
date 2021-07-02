1578fb863484959b2f8cea0ce639abd19ccc70f6
mtirtapradja <mtirtapradja@gmail.com>
2021-07-02 15:46:14 +0700

    bikin tombol login berfungsi

--- app/src/main/java/kevin/com/snapit/LoginActivity.java
+++ app/src/main/java/kevin/com/snapit/LoginActivity.java
@@ -1,14 +1,56 @@
 package kevin.com.snapit;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+import com.huawei.hmf.tasks.Task;
+import com.huawei.hms.common.ApiException;
+import com.huawei.hms.support.account.AccountAuthManager;
+import com.huawei.hms.support.account.request.AccountAuthParams;
+import com.huawei.hms.support.account.request.AccountAuthParamsHelper;
+import com.huawei.hms.support.account.result.AuthAccount;
+import com.huawei.hms.support.account.service.AccountAuthService;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = LoginActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.login_btn).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.login_btn) {
+            AccountAuthParams authParams = new AccountAuthParamsHelper(AccountAuthParams.DEFAULT_AUTH_REQUEST_PARAM).setAuthorizationCode().createParams();
+            AccountAuthService service = AccountAuthManager.getService(LoginActivity.this, authParams);
+            startActivityForResult(service.getSignInIntent(), 8888);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        // Process the authorization result to obtain the authorization code from AuthAccount.
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 8888) {
+            Task<AuthAccount> authAccountTask = AccountAuthManager.parseAuthResultFromIntent(data);
+            if (authAccountTask.isSuccessful()) {
+                // The sign-in is successful, and the user's ID information and authorization code are obtained.
+                AuthAccount authAccount = authAccountTask.getResult();
+                Log.i(TAG, "serverAuthCode:" + authAccount.getAuthorizationCode());
+            } else {
+                // The sign-in failed.
+                Log.e(TAG, "sign in failed:" + ((ApiException) authAccountTask.getException()).getStatusCode());
+            }
+        }
     }
 }
