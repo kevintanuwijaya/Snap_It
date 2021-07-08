dc121f295fe0a4bd6ffbfdd220024b254b122c80
mtirtapradja <mtirtapradja@gmail.com>
2021-07-08 11:59:35 +0700

    Aktifin MapFragment + ngerapihin xml nya

--- app/src/main/java/kevin/com/snapit/Fragment/MapFragment.java
+++ app/src/main/java/kevin/com/snapit/Fragment/MapFragment.java
@@ -4,17 +4,18 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.huawei.hms.maps.HuaweiMap;
 import com.huawei.hms.maps.MapView;
-import com.huawei.hms.maps.MapsInitializer;
 import com.huawei.hms.maps.OnMapReadyCallback;
 import com.huawei.hms.maps.SupportMapFragment;
 
-import kevin.com.snapit.MainActivity;
+import kevin.com.snapit.CategoryOnMapActivity;
 import kevin.com.snapit.R;
 
 /**
@@ -28,12 +29,13 @@ public class MapFragment extends Fragment implements View.OnClickListener, OnMap
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+    private static final String TAG = MapFragment.class.getSimpleName();
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    private SupportMapFragment mSupportMapFragment;
     private MapView mMapView;
     private HuaweiMap hMap;
 
@@ -67,18 +69,17 @@ public class MapFragment extends Fragment implements View.OnClickListener, OnMap
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-        MapsInitializer.setApiKey(String.valueOf(R.string.Api_key));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map,null);
-        //view.findViewById(R.id.current_location_btn).setOnClickListener(this);
-        mMapView = view.findViewById(R.id.mapview_mapviewdemo);
+
+        mMapView = view.findViewById(R.id.mapView);
         Bundle mapViewBundle = null;
         if(savedInstanceState != null){
-            mapViewBundle = savedInstanceState.getBundle("MapViewBundleKey");
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
         }
         mMapView.onCreate(mapViewBundle);
         mMapView.getMapAsync(this);
@@ -86,6 +87,12 @@ public class MapFragment extends Fragment implements View.OnClickListener, OnMap
         return view;
     }
 
+    @Override
+    public void onMapReady(HuaweiMap huaweiMap) {
+        hMap = huaweiMap;
+        hMap.setMyLocationEnabled(true);
+        hMap.getUiSettings().setMyLocationButtonEnabled(true);
+    }
 
     @Override
     public void onResume() {
@@ -97,8 +104,5 @@ public class MapFragment extends Fragment implements View.OnClickListener, OnMap
 
     }
 
-    @Override
-    public void onMapReady(HuaweiMap huaweiMap) {
-        hMap = huaweiMap;
-    }
+
 }
