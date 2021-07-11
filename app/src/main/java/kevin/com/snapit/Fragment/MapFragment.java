3607ddee02f4dfa2b632c420aa407ae2ebe4cbbd
mtirtapradja <mtirtapradja@gmail.com>
2021-07-11 14:28:36 +0700

    tambahin loading dialog supaya user tau kalo lagi loading, tambahin method buat dapet location user, jadi map yang muncul itu lokasi user

--- app/src/main/java/kevin/com/snapit/Fragment/MapFragment.java
+++ app/src/main/java/kevin/com/snapit/Fragment/MapFragment.java
@@ -1,21 +1,46 @@
 package kevin.com.snapit.Fragment;
 
+import android.Manifest;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.huawei.hmf.tasks.OnFailureListener;
+import com.huawei.hmf.tasks.OnSuccessListener;
+import com.huawei.hms.common.ApiException;
+import com.huawei.hms.common.ResolvableApiException;
+import com.huawei.hms.location.FusedLocationProviderClient;
+import com.huawei.hms.location.LocationCallback;
+import com.huawei.hms.location.LocationRequest;
+import com.huawei.hms.location.LocationResult;
+import com.huawei.hms.location.LocationServices;
+import com.huawei.hms.location.LocationSettingsRequest;
+import com.huawei.hms.location.LocationSettingsResponse;
+import com.huawei.hms.location.LocationSettingsStatusCodes;
+import com.huawei.hms.location.SettingsClient;
+import com.huawei.hms.maps.CameraUpdate;
+import com.huawei.hms.maps.CameraUpdateFactory;
 import com.huawei.hms.maps.HuaweiMap;
 import com.huawei.hms.maps.MapView;
 import com.huawei.hms.maps.OnMapReadyCallback;
 import com.huawei.hms.maps.SupportMapFragment;
+import com.huawei.hms.maps.model.LatLng;
+import com.huawei.hms.maps.model.MarkerOptions;
+import com.huawei.hms.site.api.model.Site;
 
 import kevin.com.snapit.CategoryOnMapActivity;
+import kevin.com.snapit.Model.LoadingDialog;
 import kevin.com.snapit.R;
 
 /**
@@ -36,6 +61,11 @@ public class MapFragment extends Fragment implements View.OnClickListener, OnMap
     private String mParam1;
     private String mParam2;
 
+    private SupportMapFragment mSupportMapFragment;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
+    private LatLng currPosition;
     private MapView mMapView;
     private HuaweiMap hMap;
 
@@ -76,6 +106,9 @@ public class MapFragment extends Fragment implements View.OnClickListener, OnMap
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map,null);
 
+        LoadingDialog loadingDialog = new LoadingDialog(getContext());
+        loadingDialog.startDialog();
+
         mMapView = view.findViewById(R.id.mapView);
         Bundle mapViewBundle = null;
         if(savedInstanceState != null){
@@ -84,7 +117,101 @@ public class MapFragment extends Fragment implements View.OnClickListener, OnMap
         mMapView.onCreate(mapViewBundle);
         mMapView.getMapAsync(this);
 
+        Log.d(TAG, "onCreateView: ");
+        dynamicPermission();
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
+        getCurrentLocation();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                moveCamera();
+                loadingDialog.dismissDialog();
+            }
+        }, 2000);
+
         return view;
+    }
+
+    private void getCurrentLocation() {
+        mLocationRequest = new LocationRequest();
+        SettingsClient settingsClient = LocationServices.getSettingsClient(getContext());
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        mLocationRequest = new LocationRequest();
+        // Set the location update interval (in milliseconds).
+        mLocationRequest.setInterval(10000);
+        // Set the location type.
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        builder.addLocationRequest(mLocationRequest);
+        LocationSettingsRequest locationSettingsRequest = builder.build();
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult != null) {
+                    double latitude = locationResult.getLastLocation().getLatitude();
+                    double longitude = locationResult.getLastLocation().getLongitude();
+                    // Process the location callback result.
+                    currPosition = new LatLng(latitude, longitude);
+                    Log.d(TAG, "onLocationResult: " + currPosition);
+                    stopTracking();
+                }
+            }
+        };
+        // Check the device location settings.
+        settingsClient.checkLocationSettings(locationSettingsRequest)
+                // Define callback for success in checking the device location settings.
+                .addOnSuccessListener(locationSettingsResponse -> {
+                    // Initiate location requests when the location settings meet the requirements.
+                    fusedLocationProviderClient
+                            .requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.getMainLooper())
+                            // Define callback for success in requesting location updates.
+                            .addOnSuccessListener(aVoid -> Log.d(TAG, "Getting your location"));
+                })
+                // Define callback for failure in checking the device location settings.
+                .addOnFailureListener(e -> {
+                    // Device location settings do not meet the requirements.
+                    int statusCode = ((ApiException) e).getStatusCode();
+                    switch (statusCode) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                            try {
+                                ResolvableApiException rae = (ResolvableApiException) e;
+                                // Call startResolutionForResult to display a pop-up asking the user to enable related permission.
+                                rae.startResolutionForResult(getActivity(), 0);
+                            } catch (IntentSender.SendIntentException sie) {
+                                // ...
+                            }
+                            break;
+                    }
+                });
+    }
+
+    private void stopTracking() {
+        fusedLocationProviderClient.removeLocationUpdates(mLocationCallback)
+                // Define callback for success in stopping requesting location updates.
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // ...
+                        Log.d(TAG, "Location update removes");
+                    }
+                })
+                // Define callback for failure in stopping requesting location updates.
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(Exception e) {
+                        // ...
+                        Log.d(TAG, "Failed to remove location update");
+                    }
+                });
+    }
+
+    private void moveCamera() {
+        LatLng pos = new LatLng(currPosition.latitude, currPosition.longitude);
+        Log.d(TAG, "moveCameraAndAddMarker: " + pos);
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(pos, 14f);
+        hMap.moveCamera(cameraUpdate);
     }
 
     @Override
@@ -104,5 +231,32 @@ public class MapFragment extends Fragment implements View.OnClickListener, OnMap
 
     }
 
+    private void dynamicPermission() {
+        // Dynamically apply for required permissions if the API level is 28 or smaller.
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            Log.i(TAG, "android sdk <= 28 Q");
+            if (ActivityCompat.checkSelfPermission(getContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(getContext(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                String[] strings =
+                        {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+                ActivityCompat.requestPermissions(getActivity(), strings, 1);
+            }
+        } else {
+            // Dynamically apply for required permissions if the API level is greater than 28. The android.permission.ACCESS_BACKGROUND_LOCATION permission is required.
+            if (ActivityCompat.checkSelfPermission(getContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(getContext(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(getContext(),
+                    "android.permission.ACCESS_BACKGROUND_LOCATION") != PackageManager.PERMISSION_GRANTED) {
+                String[] strings = {android.Manifest.permission.ACCESS_FINE_LOCATION,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                        "android.permission.ACCESS_BACKGROUND_LOCATION"};
+                ActivityCompat.requestPermissions(getActivity(), strings, 2);
+            }
+        }
+    }
 
 }
