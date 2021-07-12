abe10878251bb16024bc85f5d02af97085df20d3
KEVIN TANUWIJAYA <kevintanuwijaya11@gmail.com>
2021-07-12 12:59:25 +0700

    Tambahin object di DB

--- app/src/main/java/kevin/com/snapit/Model/Picture.java
+++ app/src/main/java/kevin/com/snapit/Model/Picture.java
@@ -15,27 +15,19 @@ import com.huawei.agconnect.cloud.database.annotations.PrimaryKeys;
 import java.util.Date;
 
 /**
- * Definition of ObjectType Users.
+ * Definition of ObjectType Picture.
  *
  * @since 2021-07-12
  */
-@PrimaryKeys({"users_email"})
-@Indexes({"users_email:users_email"})
-public final class Users extends CloudDBZoneObject {
+@PrimaryKeys({"users_email", "users_picturePath"})
+@Indexes({"users_email:users_email", "users_picturePath:users_picturePath"})
+public final class Picture extends CloudDBZoneObject {
     private String users_email;
 
-    @NotNull
-    @DefaultValue(stringValue = "name")
-    private String users_name;
+    private String users_picturePath;
 
-    @NotNull
-    @DefaultValue(stringValue = "0")
-    private String users_number;
-
-    public Users() {
-        super(Users.class);
-        this.users_name = "name";
-        this.users_number = "0";
+    public Picture() {
+        super(Picture.class);
     }
 
     public void setUsers_email(String users_email) {
@@ -46,20 +38,12 @@ public final class Users extends CloudDBZoneObject {
         return users_email;
     }
 
-    public void setUsers_name(String users_name) {
-        this.users_name = users_name;
+    public void setUsers_picturePath(String users_picturePath) {
+        this.users_picturePath = users_picturePath;
     }
 
-    public String getUsers_name() {
-        return users_name;
-    }
-
-    public void setUsers_number(String users_number) {
-        this.users_number = users_number;
-    }
-
-    public String getUsers_number() {
-        return users_number;
+    public String getUsers_picturePath() {
+        return users_picturePath;
     }
 
 }
